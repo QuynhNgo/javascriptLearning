0829e09175209fcb3ebdce3494dfac3a1fe46f95
ngoq <ngoq@exchange.wwu.de>
2019-07-06 23:28:42 +0200

    updateD3

--- helloWorld.js
+++ helloWorld.js
@@ -1,10 +1,25 @@
 
+import * as d3 from d3;
+
 function Dosomething()
 {
     console.log("Hello, it's me who is learning js");
-    d3.json('https://jsonplaceholder.typicode.com/todos/1')
-    .then(data =>{console.log(data);})
+    d3.json("data.json", function(data)
+    {
+        var canvas = d3.select("body")
+        .append("svg")
+        .attr("width", 500)
+        .attr("height", 500);
+    
+        canvas.selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("width",function(d){return d.age * 10;})
+        .attr("height",48)
+        .attr("y", function (d,i){return i * 50;})
+        .attr("fill","blue")
+    })
+    
 }
-
-Dosomething();
 
