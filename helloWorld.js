b4a037e21e355c423b7bc5ce73c994e89c687260
ngoq <ngoq@exchange.wwu.de>
2019-07-06 16:57:55 +0200

    onemore

--- helloWorld.js
+++ helloWorld.js
@@ -1,8 +1,7 @@
 var smg = "hello world";
 console.log(smg)
 
-
-void Dosomething()
+function Dosomething()
 {
     console.log("Hello, it's me who is learning js");
     d3.json('https://jsonplaceholder.typicode.com/todos/1')
