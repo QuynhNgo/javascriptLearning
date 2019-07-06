35655711cc71f7a6c86e3a1475421f06dac41d77
ngoq <ngoq@exchange.wwu.de>
2019-07-06 15:15:22 +0200

    with json data in d3

--- helloWorld.js
+++ helloWorld.js
@@ -1,3 +1,7 @@
+
+<script scr = "https://cdnjs.cloudflare.com/ajax/libs/d3/5.9.2/d3.min.js"> </script>
+
+
 var smg = "hello world";
 console.log(smg)
 
@@ -5,4 +9,13 @@ console.log(smg)
 void Dosomething()
 {
     console.log("Hello, it's me who is learning js");
+    d3.json('https://jsonplaceholder.typicode.com/todos/1')
+    .then(data =>{console.log(data);})
+}
+
+void dokdjdlj()
+{
+    skdjfkajfkj
+    lakjfdkjk
+    aldjkj
 }
